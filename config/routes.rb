0a7ca294645cf7028a3c6437cdaa977505590dab
himanushi <himanushi@users.noreply.github.com>
2023-01-14 19:09:38 +0900

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
